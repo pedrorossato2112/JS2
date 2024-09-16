1d70a10b988d687888a4ca55519eefb075e662be
Rossato_Pedr1n <rossato.pedro@escola.pr.gov.br>
2024-09-16 11:47:05 -0300

    teste

---
+++ script.js
@@ -0,0 +1,27 @@
+function tocasompom (){
+    document.querySelector('#som_tecla_pom').play()
+}
+function tocasompom (){
+    document.querySelector('#som_tecla_clap').play()
+}
+function tocasompom (){
+    document.querySelector('#som_tecla_tim').play()
+}
+function tocasompom (){
+    document.querySelector('#som_tecla_puff').play()
+}
+function tocasompom (){
+    document.querySelector('#som_tecla_splash').play()
+}
+function tocasompom (){
+    document.querySelector('#som_tecla_toim').play()
+}
+function tocasompom (){
+    document.querySelector('#som_tecla_psh').play()
+}
+function tocasompom (){
+    document.querySelector('#som_tecla_tic').play()
+}
+function tocasompom (){
+    document.querySelector('#som_tecla_tom').play()
+}
